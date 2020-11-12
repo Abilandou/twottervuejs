128802923b125c7e6f16bf0f01f78ead7239615b
Godlove Abilandou <abilandougodlove@gmail.com>
2020-11-12 01:39:33 +0100

    project created + single file component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+      <p>{{ user.username }} - {{ fullName }}</p>
+      <strong>Followers---{{ followers }}</strong>
+
+      <button @click="followUser">
+        Follow
+      </button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'Godlove',
+        firstname: 'Abiland',
+        email: 'godlove@gmail.com',
+        is_admin: true
+      }
+    }
+  },
+
+  watch: {
+    followers(newFollowerCount, oldFollowerCount){
+      if(oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a new follower!`)
+      }
+    }
+  },
+
+  computed: {
+    fullName(){
+      return `${this.user.firstname} ${this.user.username}`
+    }
+  },
+
+  methods: {
+    followUser(){
+      this.followers++
+    }
+  },
+
+  mounted(){
+    this.followUser();
   }
 }
 </script>
@@ -22,5 +60,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
 }
 </style>
